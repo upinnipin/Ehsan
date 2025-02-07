ea96ba07afaaf973738ee502243823ba091e2a19
upinnipin <syahidaheru@gmail.com>
2025-02-07 05:47:24 +0000

    1

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-from flask import flask
+from flask import Flask, request, render_template # type: ignore
+
+app = Flask(__name__)
+
+# home route that returns below text when root url is accessed
+@app.route("/", methods=['GET','POST'])
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":  
+   app.run()
